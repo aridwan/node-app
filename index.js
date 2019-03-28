1b2a80c954ee481e4f13cfc8a35737cad27533ec
Arief Ridwan <aridwan05@gmail.com>
2019-03-28 13:36:25 +0700

    harus sampe deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('uji coba');
+ res.send('hello world');
  //add comment
 });
 //listen to port 3000 by default
